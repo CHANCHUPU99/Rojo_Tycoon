d648bd2db37b34882f4911f6ddddea69d8c93678
GilbertoParedezzz <angel.paredes@artek.edu.mx>
2022-11-24 12:54:16 -0600

    menu ahora bn

--- Rojo_Tycoon/Assets/Scripts/MenuScript.cs
+++ Rojo_Tycoon/Assets/Scripts/MenuScript.cs
@@ -6,14 +6,14 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void StartGEIM()
+    public void StartGEIM()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     // Update is called once per frame
-    void ExitGeim()
+    public void ExitGeim()
     {
-        
+        Application.Quit();
     }
 }
