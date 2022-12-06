09f8af8aab8a27e508a8dd28a5b2439624729e37
CHANCHUPU99 <gal.frias@gmail.com>
2022-12-06 12:39:20 -0600

    prefab terminado

--- Rojo_Tycoon/Assets/Scripts/PrefabInstantiation.cs
+++ Rojo_Tycoon/Assets/Scripts/PrefabInstantiation.cs
@@ -15,20 +15,21 @@ public class PrefabInstantiation : MonoBehaviour
     public Transform[] CardPositionOne;
     public Transform[] CardPositionTwo;
 
-    public List<GameObject> prefabList;
+    public List<GameObject> prefabListOne;
+    public List<GameObject> prefabListTwo;
 
     //public TeamSelection teamSelection;
 
     // Start is called before the first frame update
     public void InstantiateID()
     {
-        prefabList = new List<GameObject>();
+        prefabListOne = new List<GameObject>();
         for (int i = 0; i < CardPositionOne.Length; i++)
         {
             //GameObject m_Instance = Instantiate(prefab, CardPositionOne[i].position, Quaternion.identity,
             //parentPrefab.transform) as GameObject;
 
-            prefabList.Add(Instantiate(prefab, CardPositionOne[i].position, Quaternion.identity,
+            prefabListOne.Add(Instantiate(prefab, CardPositionOne[i].position, Quaternion.identity,
             parentPrefab.transform) as GameObject);
 
             print("Se instanció un prefab");
@@ -36,10 +37,24 @@ public class PrefabInstantiation : MonoBehaviour
             //nameText[0].text = "Se instanció un prefab";
         }
 
-        for(int i= 0; i < prefabList.Count; i++)
+        for(int i= 0; i < prefabListOne.Count; i++)
         {
-            prefabList[i].GetComponent<CardObjectText>().nameText.text = TeamSelection.instance.teamOne[i].name;
-            prefabList[i].GetComponent<CardObjectText>().descriptionText.text = TeamSelection.instance.teamOne[i].personality;
+            prefabListOne[i].GetComponent<CardObjectText>().nameText.text = TeamSelection.instance.teamOne[i].name;
+            prefabListOne[i].GetComponent<CardObjectText>().descriptionText.text = TeamSelection.instance.teamOne[i].personality;
+
+        }
+
+        prefabListTwo = new List<GameObject>();
+        for (int i = 0; i < CardPositionTwo.Length; i++)
+        {
+            prefabListTwo.Add(Instantiate(prefab, CardPositionTwo[i].position, Quaternion.identity,
+            parentPrefab.transform) as GameObject);
+        }
+
+        for (int i = 0; i < prefabListTwo.Count; i++)
+        {
+            prefabListTwo[i].GetComponent<CardObjectText>().nameText.text = TeamSelection.instance.teamTwo[i].name;
+            prefabListTwo[i].GetComponent<CardObjectText>().descriptionText.text = TeamSelection.instance.teamTwo[i].personality;
 
         }
 
